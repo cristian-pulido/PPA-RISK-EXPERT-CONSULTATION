752c7f8bb6196f39750aa5b853294c05dae8b45b
cristian-pulido <cpulido@unal.edu.co>
2026-09-19 16:13:37 -0500

    chore: simplify public consultation page

--- app-config.js
+++ app-config.js
@@ -1,6 +1,3 @@
-// La URL de biblioteca identifica el código publicado en Apps Script.
-// No se debe usar como iframe: el formulario necesita una URL de aplicación web /exec.
 window.PPA_APP_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbz4FPJ-WzvmoGZOC8KdtxD7FZ41ywMcEfdlrpiLez7Sb1mnqEHZ0HF3vbv4S6mvQ8VRnA/exec",
-  appsScriptLibraryUrl: "https://script.google.com/macros/library/d/1RPXCd3EvLSJiYOe02_tFrOpFK-QtF28OKXfgGeeS4kRIMFGx9cDs92V5/1"
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbz4FPJ-WzvmoGZOC8KdtxD7FZ41ywMcEfdlrpiLez7Sb1mnqEHZ0HF3vbv4S6mvQ8VRnA/exec"
 };
